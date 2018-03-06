db8b1104757866926aaa4a8b37c24819cd7a4ebd
arti <searchzuck2013@gmail.com>
2018-03-06 22:12:34 +0545

    condotiion loop in python

--- helloworld.py
+++ helloworld.py
@@ -20,13 +20,34 @@ print (datetime.now())
 
 #Tested the String 
 s = 'hi'
-print s[1]          ## i
-print len(s)        ## 2
-print s + ' there'  ## hi there
+print (s[1])          ## i
+print (len(s))        ## 2
+print (s + ' there')  ## hi there
 
 pi = 3.14
 ##text = 'The value of pi is ' + pi      ## NO, does not work
 text = 'The value of pi is '  + str(pi)  ## yes
 
-print text  #The value of pi is 3.14
+print (text)  #The value of pi is 3.14
+
+# get input from  user 
+myname=input("What is your name?")
+#print (myname)  ## execute this statement by python3 filename 
+
+myvar='test'
+print (myvar)
+
+#conditional execution  execute by python3 file name
+if(myname=="james"):
+            print("james is in form")
+else :
+     print ("no one is great")           
+
+
+if(myvar=='test'):
+    print ("test")
+
+else :
+    print ("no test")  
+
 
